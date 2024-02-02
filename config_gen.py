57dbab3567de084023c26f9f5011a31488385bab
czylabsonasa <czylabson.asa@gmail.com>
2024-02-02 13:27:53 +0100

    progress...

--- config_gen.py
+++ config_gen.py
@@ -1,4 +1,16 @@
-FLOAT_TOL=1e-6
+# modify the executable path according to your system
+# using raw string one can avoid escaping \
+# on windows put here the full paths of the executables
+python_exe   =   r"python"
+matlab_exe   =   r"matlab"
+octave_exe   =   r"octave-cli"
+julia_exe    =   r"julia"
+golang_exe   =   r"go"
+nodejs_exe   =   r"node"
+
+######################################################################################
+
+FLOAT_TOL=1e-4
 INT_TOL=1e-16 # func módban probléma lehet, mert az app.m mindent lebegopontosan ir ki
 
 
@@ -38,23 +50,50 @@ _ext2lang=dict({
 
 # func mod parancsai
 _fvege=" 1> fake.out 2> fake.out"
+def mkFunMode(exe, args):
+  return "{} {} {}".format(exe, args, _fvege)
+
+# _lang2cmdF=dict({
+    # "python"  :"python3 app.py"+_fvege,
+    # "matlab"  :"matlab -nojvm -nodesktop -nosplash < app.m"+_fvege,
+    # "octave"  :"octave-cli app.m"+_fvege,
+    # "julia"   :"julia app.jl"+_fvege,
+    # "node.js" :"node app.js "+_fvege
+# })
+
+
 _lang2cmdF=dict({
-    "python"  :"python3 app.py"+_fvege,
-    "matlab"  :"matlab -nojvm -nodesktop -nosplash < app.m"+_fvege,
-    "octave"  :"octave-cli app.m"+_fvege,
-    "julia"   :"julia app.jl"+_fvege,
-    "node.js" :"node app.js "+_fvege
+    "python"  : mkFunMode(python_exe, "app.py"),
+    "matlab"  : mkFunMode(matlab_exe, "-nojvm -nodesktop -nosplash < app.m"),
+    "octave"  : mkFunMode(octave_exe, "app.m"), 
+    "julia"   : mkFunMode(julia_exe,  "app.jl"),
+    "node.js" : mkFunMode(nodejs_exe, "app.js"),
 })
 
+
 _pvege="< io/{:s}.in 1> act/{:s}.out 2> fake.out"
+def mkProgMode(exe, args):
+  return "{} {} {}".format(exe, args, _pvege)
+
+
 _lang2cmdP=dict({
-    "python"  :"python3 solve.py "+_pvege,
-    "octave"  :"octave-cli solve.m "+_pvege,
-    "julia"   :"julia solve.jl "+_pvege,
-    "node.js" :"node solve.js "+_pvege,
-    "golang"  :"go run solve.go "+_pvege,
-    "binary"  :"./solve.exe "+_pvege # win ???
-})
+    "python"  : mkProgMode(python_exe, "solve.py"),
+    "octave"  : mkProgMode(octave_exe, "solve.m"), 
+    "julia"   : mkProgMode(julia_exe,  "solve.jl"),
+    "node.js" : mkProgMode(nodejs_exe, "solve.js"),
+    "golang"  : mkProgMode(golang_exe, "run solve.go"),
+    "binary"  : mkProgMode("", "./solve.exe"),
+})        
+
+
+# _lang2cmdP=dict({
+    # "python"  :"python3 solve.py "+_pvege,
+    # "octave"  :"octave-cli solve.m "+_pvege,
+    # "julia"   :"julia solve.jl "+_pvege,
+    # "node.js" :"node solve.js "+_pvege,
+    # "golang"  :"go run solve.go "+_pvege,
+    # "binary"  :"./solve.exe "+_pvege # win ???
+# })
 
 _modes=["prog","func"]
 
